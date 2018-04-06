1dbe86326c7a9d25701d99538567a7c9db8be5a2
Ruslan <4ruslan.k@gmail.com>
2018-04-07 02:01:12 +0500

    buttons

--- src/app/allbooks/allbooks.component.ts
+++ src/app/allbooks/allbooks.component.ts
@@ -15,6 +15,10 @@ export class AllbooksComponent implements OnInit {
   approvedOutgoingBooks = [];
   incomingRequestedBooks = [];
   approvedIncomingBooks = [];
+  outgoingRequestsNum = 0;
+  incomingRequestsNum = 0;
+  isHiddenIncoming = true;
+  isHiddenOutgoing = true;
 
   constructor(private bookService: BookService,
     private authService: AuthService) { }
@@ -26,6 +30,25 @@ export class AllbooksComponent implements OnInit {
     this.getIncomingRequested();
     this.getApprovedOutgoing();
   }
+
+  showIncoming() {
+    if (this.isHiddenIncoming) {
+      this.isHiddenIncoming = false
+    }
+    else {
+      this.isHiddenIncoming = true
+    }
+  }
+
+  showOutgoing() {
+    if (this.isHiddenOutgoing) {
+      this.isHiddenOutgoing = false
+    }
+    else {
+      this.isHiddenOutgoing = true
+    }
+  }
+
 
 
   getBooks() {
@@ -49,6 +72,7 @@ export class AllbooksComponent implements OnInit {
         data => {
           console.log('requestedBooks: ' + data)
           this.requestedBooks = data;
+          this.outgoingRequestsNum = this.outgoingRequestsNum + this.requestedBooks.length
         },
         error => {
           console.log(error);
@@ -61,6 +85,7 @@ export class AllbooksComponent implements OnInit {
         data => {
           console.log('getApprovedIncoming: ' + data)
           this.approvedOutgoingBooks = data;
+          this.outgoingRequestsNum = this.outgoingRequestsNum + this.approvedOutgoingBooks.length
         },
         error => {
           console.log(error);
@@ -74,6 +99,7 @@ export class AllbooksComponent implements OnInit {
         data => {
           console.log('incomingRequestedBooks: ' + data)
           this.incomingRequestedBooks = data;
+          this.incomingRequestsNum = this.incomingRequestsNum + this.incomingRequestedBooks.length
         },
         error => {
           console.log(error);
@@ -86,6 +112,7 @@ export class AllbooksComponent implements OnInit {
         data => {
           console.log('approvedIncomingBooks: ' + data)
           this.approvedIncomingBooks = data;
+          this.incomingRequestsNum = this.incomingRequestsNum + this.approvedIncomingBooks.length
         },
         error => {
           console.log(error);
@@ -104,6 +131,7 @@ export class AllbooksComponent implements OnInit {
               book.isRequested = true
             }
           }
+          this.outgoingRequestsNum += 1
         },
         error => {
           console.log(error);
@@ -123,6 +151,7 @@ export class AllbooksComponent implements OnInit {
           for (let i = 0; i < this.requestedBooks.length; i++) {
             if (this.requestedBooks[i]._id == id) {
               this.requestedBooks.splice(i, 1)
+              this.outgoingRequestsNum -= 1
             }
           }
         },
@@ -144,6 +173,7 @@ export class AllbooksComponent implements OnInit {
           for (let i = 0; i < this.approvedOutgoingBooks.length; i++) {
             if (this.approvedOutgoingBooks[i]._id == id) {
               this.approvedOutgoingBooks.splice(i, 1)
+              this.outgoingRequestsNum -= 1
             }
           }
         },
@@ -165,6 +195,7 @@ export class AllbooksComponent implements OnInit {
           for (let i = 0; i < this.incomingRequestedBooks.length; i++) {
             if (this.incomingRequestedBooks[i]._id == id) {
               this.incomingRequestedBooks.splice(i, 1)
+              this.incomingRequestsNum -= 1
             }
           }
         },
@@ -186,6 +217,7 @@ export class AllbooksComponent implements OnInit {
           for (let i = 0; i < this.approvedIncomingBooks.length; i++) {
             if (this.approvedIncomingBooks[i]._id == id) {
               this.approvedIncomingBooks.splice(i, 1)
+              this.incomingRequestsNum -= 1
             }
           }
         },
